96b24b9915854844f6c9ae450f3a279b22d7954f
DimaOanaTeodora <oanadima26@gmail.com>
2022-08-16 19:01:00 +0300

    Adding sprites

---
+++ README.md
@@ -0,0 +1,24 @@
+# Timber
+screen resolution is 1,920 x 1,080
+
+- Download SFML [here](https://www.sfml-dev.org/download/sfml/2.5.1/)
+- Microsfot Visual Studio 2022 
+
+This project uses the simplest possible version of a game loop, every game will need these phases in the code:
+
+1. Get the player’s input (if any).
+2. Update the scene based on things such as artificial intelligence, physics, or the player’s input.
+3. Draw the current scene.
+4. Repeat these steps at a fast-enough rate to create a smooth, animated game world.
+
+- **The player’s score**: Each time the player chops a log, they will get one point. They can chop a log with either the left or the right arrow (cursor) key.
+- **Player character**: Each time the player chops, they will move to/stay on the same side of the tree relative to the cursor key they use. Therefore, the player must be careful which side they choose to chop on.
+- When the player chops, a simple axe graphic will appear in the player character’s hands.
+- **Shrinking time-bar**: Each time the player chops, a small amount of time will be added to the ever-shrinking time-bar.
+- **The lethal branches**: The faster the player chops, the more time they will get, but also the faster the branches will move down the tree and therefore the more likely they are to get squished. The branches spawn randomly at the top of the tree and move down with each chop.
+- When the player gets squished – and they will get squished quite regularly – a gravestone graphic will appear.
+- **The chopped log**: When the player chops, a chopped log graphic will whiz off, away from the player.
+- **Just for decoration**: There are three floating clouds that will drift at random heights and speeds, as well as a bee that does nothing but fly around.
+- **The background**: All this takes place on a pretty background.
+
+*So, in a nutshell, the player must frantically chop to gain points and avoid running out of time. As a slightly perverse, but fun consequence, the faster they chop, the more likely their squishy demise is.*
--- Timber.cpp
+++ Timber.cpp
@@ -1,20 +1,230 @@
-// Timber.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <sstream>
+using namespace sf;
+// This is where our game starts from
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    VideoMode vm(1920, 1080);
+    RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+  
+    // Background
+    Texture textureBackground;
+    textureBackground.loadFromFile("graphics/background.png");
+    Sprite spriteBackground;
+    spriteBackground.setTexture(textureBackground);
+    spriteBackground.setPosition(0, 0);
+
+    // Tree
+    Texture textureTree;
+    textureTree.loadFromFile("graphics/tree.png");
+    Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(810, 0);
+
+    // Moving Bee
+    Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+    Sprite spriteBee;
+    spriteBee.setTexture(textureBee);
+    spriteBee.setPosition(0, 800);
+    bool beeActive = false; // Is the bee currently moving?
+    float beeSpeed = 0.0f; // How fast can the bee fly
+
+    // Moving 3 clouds
+    Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
+    Sprite spriteCloud1;
+    Sprite spriteCloud2;
+    Sprite spriteCloud3;
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud3.setTexture(textureCloud);
+    spriteCloud1.setPosition(0, 0);
+    spriteCloud2.setPosition(0, 250);
+    spriteCloud3.setPosition(0, 500);
+    bool cloud1Active = false;    // Are the clouds currently on screen?
+    bool cloud2Active = false;
+    bool cloud3Active = false;
+    float cloud1Speed = 0.0f;
+    float cloud2Speed = 0.0f;
+    float cloud3Speed = 0.0f;
+
+    Clock clock;
+    // Time bar
+    RectangleShape timeBar;
+    float timeBarStartWidth = 400;
+    float timeBarHeight = 80;
+    timeBar.setSize(Vector2f(timeBarStartWidth, timeBarHeight));
+    timeBar.setFillColor(Color::Red);
+    timeBar.setPosition((1920 / 2) - timeBarStartWidth / 2, 980);
+    Time gameTimeTotal;
+    float timeRemaining = 6.0f;
+    float timeBarWidthPerSecond = timeBarStartWidth / timeRemaining;
+
+    // Paused
+    bool paused = true;
+
+    // Message text
+    Font font;
+    font.loadFromFile("fonts/KOMIKAP_.ttf");
+
+    Text messageText;
+    messageText.setFont(font);
+    messageText.setString("Press Enter to start!");
+    messageText.setCharacterSize(75);
+    messageText.setFillColor(Color::White);
+
+    // score text
+    int score = 0;
+    Text scoreText;
+    scoreText.setFont(font);
+    scoreText.setString("Score = 0");
+    scoreText.setCharacterSize(100);
+    scoreText.setFillColor(Color::White);
+
+    // Position the text
+    FloatRect textRect = messageText.getLocalBounds();
+    messageText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+    messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+    scoreText.setPosition(20, 20);
+
+    while(window.isOpen())
+    {
+        // handle the players input
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+            window.close();
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Return))
+        {
+            paused = false;
+        }
+        // update the scene
+        if (!paused)
+        {
+            Time dt = clock.restart(); // restart the clock every frame
+            if (!beeActive)
+            {
+                // speed
+                srand((int)time(0));
+                beeSpeed = (rand() % 200) + 200;
+
+                // hight
+                srand((int)time(0) * 10);
+                float height = (rand() % 500) + 500;
+                spriteBee.setPosition(2000, height);
+
+                beeActive = true;
+            }
+            else
+            {
+                // move the bee
+                spriteBee.setPosition(spriteBee.getPosition().x - (beeSpeed * dt.asSeconds()), spriteBee.getPosition().y);
+
+                // Has the bee reached the left-hand edge of the screen?
+                if (spriteBee.getPosition().x < -100)
+                {
+                    beeActive = false;
+                }
+            }
+            // Cloud 1
+            if (!cloud1Active)
+            {
+                srand((int)time(0) * 10);
+                cloud1Speed = (rand() % 200);
+
+                srand((int)time(0) * 10);
+                float height = (rand() % 150);
+                spriteCloud1.setPosition(-200, height);
+
+                cloud1Active = true;
+            }
+            else
+            {
+                spriteCloud1.setPosition(spriteCloud1.getPosition().x + (cloud1Speed * dt.asSeconds()), spriteCloud1.getPosition().y);
+
+                // Has the cloud reached the right hand edge of the screen?
+                if (spriteCloud1.getPosition().x > 1920)
+                {
+                    cloud1Active = false;
+                }
+            }
+            // Cloud 2
+            if (!cloud2Active)
+            {
+
+                srand((int)time(0) * 20);
+                cloud2Speed = (rand() % 200);
+
+                srand((int)time(0) * 20);
+                float height = (rand() % 300) - 150;
+                spriteCloud2.setPosition(-200, height);
+
+                cloud2Active = true;
+            }
+            else
+            {
+                spriteCloud2.setPosition(spriteCloud2.getPosition().x + (cloud2Speed * dt.asSeconds()), spriteCloud2.getPosition().y);
+
+                // Has the cloud reached the right hand edge of the screen?
+                if (spriteCloud2.getPosition().x > 1920)
+                {
+                    cloud2Active = false;
+
+                }
+            }
+            // Cloud 3
+            if (!cloud3Active)
+            {
+
+                srand((int)time(0) * 30);
+                cloud3Speed = (rand() % 200);
+
+                srand((int)time(0) * 30);
+                float height = (rand() % 450) - 150;
+                spriteCloud3.setPosition(-200, height);
+
+                cloud3Active = true;
+            }
+            else
+            {
+                spriteCloud3.setPosition(spriteCloud3.getPosition().x + (cloud3Speed * dt.asSeconds()), spriteCloud3.getPosition().y);
+
+                // Has the cloud reached the right hand edge of the screen?
+                if (spriteCloud3.getPosition().x > 1920)
+                {
+                    cloud3Active = false;
+                }
+            }
+
+            // Update the score text
+            std::stringstream ss;
+            ss << "Score = " << score;
+            scoreText.setString(ss.str());
+        }
+        // draw the scene
+        
+        // clear everything from the last frame
+        window.clear();
+
+        // draw our game scene 
+        window.draw(spriteBackground);
+        window.draw(spriteCloud1);
+        window.draw(spriteCloud2);
+        window.draw(spriteCloud3);
+        window.draw(spriteTree);
+        window.draw(spriteBee);
+        window.draw(scoreText);
+        if (paused)
+        {
+            window.draw(messageText);
+        }
+
+        // show everything we just drew
+        window.display();
+    }
+    return 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
